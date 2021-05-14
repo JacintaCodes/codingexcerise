704086ccb94d52f32e0e823df0952b0d46311c4b
jacinta246 <jacinta246@yahoo.com>
2021-05-14 17:06:41 -0400

    clean code

---
+++ codingexcerise.js
@@ -0,0 +1,23 @@
+def f(){
+    if A:
+        z()
+        if B:
+            y()
+            if C:
+                x()
+                if D:
+                    w()
+                     return True
+               
+                v()
+                return False
+
+            u()
+            return False
+
+        t()
+        return False
+       
+    s()
+    return False
+}
